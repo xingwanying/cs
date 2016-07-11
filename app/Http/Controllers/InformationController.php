94d5c89bca1893684cd6c0aa39954be1dceb70fc
xingwanying <xwy.1234@163.com>
2016-07-11 10:55:53 +0800

    完成编辑功能

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\User;
-use App\Infomation;
+use App\Information;
 use Response,Form;
 use DB, Hashids, Log, Redirect, Input, Config, Hash, Validator, Gate;
 use Auth;
@@ -21,12 +21,12 @@ class InformationController extends Controller
             return Redirect::to("/auth/login")
                 ->withErrors(["login.failed" => "请先登录"]);
         }elseif($user->role == 110){
-            $info_output = Infomation::orderBy('updated_at', 'desc')->select('id', 'title','cover_img_url', 'updated_at')->paginate(11);
+            $info_output = information::orderBy('updated_at', 'desc')->select('id', 'title','cover_img_url', 'updated_at')->paginate(11);
             $info_output = $info_output->toArray();
             $info_data['current_page'] = $info_output['current_page'];
             $info_data['last_page'] = $info_output['last_page'];
             for( $info_int=0; $info_int<count($info_output['data']); $info_int++){
-                $info_output['data'][$info_int]['cover_img_url'] = Config::get("cuc.www_host") . $info_output['data'][$info_int]['cover_img_url'];
+                $info_output['data'][$info_int]['cover_img_url'] = Config::get("cuc.www_host") . '/' . $info_output['data'][$info_int]['cover_img_url'];
             }
             $info_data['data'] =  $info_output['data'];
             return view("cms.info")->with('info',$info_output);
@@ -66,7 +66,7 @@ class InformationController extends Controller
 
             $validator = Validator::make($request->all(), $rules);   //验证输入信息的合法性
             if ($validator->passes()) {
-                $vObject = new Infomation();
+                $vObject = new information();
                 $uploaded_img = $request->file('photo');
                 $title = $request->get("title");
                 $type = $request->get("type");
@@ -83,7 +83,7 @@ class InformationController extends Controller
                     Log::debug("saveType:" .  $img_ext);
 
                     if (empty($img_ext)) {
-                        abort(422); // TODO 返回错误信息提示页面
+                        return Redirect::to("information/create")->withErrors(["create.failed" => "图片类型错误"]);
                     }else{
 
                         $uri_prefix_img = "information/" . Hashids::connection("information")->encode($usid);  //获得唯一图文封面的文件地址
@@ -98,7 +98,7 @@ class InformationController extends Controller
                         $uploaded_img->move($saveToDir_img, $img_file_name);   //保存文件
                         Log::debug("saveToDir:" .  $uploaded_img);
 
-                        $vObject->cover_img_url =  '/upload/img/' . $uri_prefix_img . DIRECTORY_SEPARATOR . $img_file_name;
+                        $vObject->cover_img_url =  'upload/img/' . $uri_prefix_img . DIRECTORY_SEPARATOR . $img_file_name;
                         Log::debug("cover_img_url:" .  $vObject->cover_img_url);
 
                     }
@@ -141,8 +141,8 @@ class InformationController extends Controller
             return Redirect::to("/auth/login")
                 ->withErrors(["login.failed" => "请先登录"]);
         }elseif($user->role == 110){
-            $info_output = Infomation::find($id);
-            $info_output['cover_img_url'] =  Config::get("cuc.www_host") . $info_output['cover_img_url'];
+            $info_output = information::find($id);
+            $info_output['cover_img_url'] =  Config::get("cuc.www_host") . '/' . $info_output['cover_img_url'];
             if($info_output){
                 $info_output['html_url'] =  public_path( $info_output['html_url']);
                 if (file_exists($info_output['html_url'])) {
@@ -162,21 +162,99 @@ class InformationController extends Controller
         }
 
     }
-    public function postEdit($id,Request $request){
+    public function postEdit(Request $request,$id){
         $user = Auth::user();
         if (empty($user)){
             return Redirect::to("/auth/login")
                 ->withErrors(["login.failed" => "请先登录"]);
         }elseif($user->role == 110){
-            $info_output = Infomation::orderBy('updated_at', 'desc')->select('id', 'title','cover_img_url', 'updated_at')->paginate(11);
-            $info_output = $info_output->toArray();
-            $info_data['current_page'] = $info_output['current_page'];
-            $info_data['last_page'] = $info_output['last_page'];
-            for( $info_int=0; $info_int<count($info_output['data']); $info_int++){
-                $info_output['data'][$info_int]['cover_img_url'] = Config::get("cuc.www_host") . $info_output['data'][$info_int]['cover_img_url'];
+            $rules = [
+                'content' => "max:2000",
+                'title' => "max:200",
+                'photo' => "image",
+                'type' => "numeric",
+            ];
+            $validator = Validator::make($request->all(), $rules);   //验证输入信息的合法性
+            if ($validator->passes()) {
+                $vObject = Information::find($id);
+                if($vObject){
+                    $uploaded_img = $request->file('photo');
+                    $title = $request->get("title");
+                    $type = $request->get("type");
+                    $content = $request->get("content");        //获取html文件的内容
+                    $usid = $user -> id;
+                    if ($uploaded_img) {
+                        $input_img = $uploaded_img->getFileInfo();
+
+                        // TODO 检查相同图片（hash值相同）是否重复上传
+                        // 根据文件类型“猜测”文件名后缀，$img_ext为img等
+                        $mime_type = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $input_img);
+                        $img_ext = Config::get("cuc.mime2ext." . $mime_type);
+                        Log::debug("saveType:" .  $img_ext);
+
+                        if (empty($img_ext)) {
+                            return Redirect::to("information/edit/" . $id)->withErrors(["edit.failed" => "图片类型错误"]);
+
+                        }else{
+
+                            $uri_prefix_img = "information/" . Hashids::connection("information")->encode($usid);  //获得唯一图文封面的文件地址
+                            $saveToDir_img = public_path('upload/img/' . $uri_prefix_img);   //获得完整的路径
+                            if (!is_dir($saveToDir_img)) {
+                                mkdir($saveToDir_img, 0755, true);
+                            }
+                            $img_hashid = Hashids::connection("main")->encode(preg_replace('/\./', '', microtime(true)));   //利用时间戳获得文件名
+                            Log::debug("$img_hashid");
+                            $img_file_name = $img_hashid . '.' . $img_ext;    //联合文件名和后缀
+
+                            $uploaded_img->move($saveToDir_img, $img_file_name);   //保存文件
+                            Log::debug("saveToDir:" .  $uploaded_img);
+
+                            $vObject->cover_img_url =  '/upload/img/' . $uri_prefix_img . DIRECTORY_SEPARATOR . $img_file_name;
+                            Log::debug("cover_img_url:" .  $vObject->cover_img_url);
+
+                        }
+
+                    }
+
+                    $uri_prefix_h5 =  Hashids::connection("information")->encode($usid);  //获得唯一百科的文件地址
+                    $saveToDir_h5 = public_path('upload/h5/' . $uri_prefix_h5);//获得完整的路径
+                    Log::debug("saveToDir:" . $saveToDir_h5);
+                    if (!is_dir($saveToDir_h5)) {
+                        mkdir($saveToDir_h5, 0755, true);
+                    }
+                    $h5_hashid = Hashids::connection("main")->encode(preg_replace('/\./', '', microtime(true)));  //利用时间戳获得文件名
+                    Log::debug("$h5_hashid");
+                    $h5_file_name = $h5_hashid . "." . "html";  //联合文件名和后缀
+                    touch($saveToDir_h5 . "/" . $h5_file_name);   //创建文件
+//                $content = "<meta charset='utf-8'><meta name='viewport' content='initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width'>" . $content;//拼接html的head
+                    file_put_contents($saveToDir_h5 . "/" . $h5_file_name, $content);   //将html文件的内容保存到对应文件中
+
+                    $vObject -> html_url = 'upload/h5/' .  $uri_prefix_h5 . DIRECTORY_SEPARATOR . $h5_file_name;
+                    $vObject -> title = $title;
+                    $vObject -> op_uid = $usid;
+                    $vObject -> type = $type;
+                    $vObject->save();
+                    //删除旧的html和img文件
+                    $old_html = public_path('upload/h5' . $vObject->html_url);
+                    if(file_exists($old_html)){
+                        unlink($old_html);
+                    }
+                    $old_img = public_path('upload/img' . $vObject->cover_img_url);
+                    if(file_exists($old_img)){
+                        unlink($old_img);
+                    }
+                    return Redirect::to("information/show")->withStatus("修改成功");
+
+                }else{
+                return Redirect::to("information/show")
+                    ->withErrors(["exit.failed" => "图文不存在"]);
             }
-            $info_data['data'] =  $info_output['data'];
-            return view("cms.info")->with('info',$info_output);
+
+
+            } else {
+                return Redirect::to("information/edit/" . $id)->withErrors(["edit.failed" => "数据验证不通过"]);
+
+            }
         }else {
             return Redirect::to("/auth/login")
                 ->withErrors(["login.failed" => "禁止越权使用"]);
--- app/Information.php
+++ app/Information.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Infomation extends Model
+class Information extends Model
 {
     //
     protected $table = 'informations';
